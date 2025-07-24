4e2803098666b46db2e1c579e8b24f1df0074ae0
Blessing516 <blessingmanamela05@gmail.com>
2025-07-24 19:11:18 +0200

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const chatbotDiv = document.getElementById('chatbot');
+  chatbotDiv.innerHTML = '<p>Hello! I am BIZSELLAI Bot 🤖. What do you want to buy today?</p>';
+});
